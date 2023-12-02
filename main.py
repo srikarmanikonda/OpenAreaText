04c6852764f44a9be47eee272170b184ce569200
Srikar Manikonda <avatsara3@gmail.com>
2023-12-01 22:19:08 -0600

    add rate limit to api

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify, abort
-
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"]
+)
 
 #  area codes data
 
